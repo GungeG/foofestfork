61641df40aac8643565f0a92c311df066c9e15ee
axwii <axwi0001@stud.kea.dk>
2025-01-15 02:00:56 +0100

    feat: enhance PaymentForm component with improved validation and error handling

--- src/app/components/PaymentCard.jsx
+++ src/app/components/PaymentCard.jsx
@@ -3,74 +3,94 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-const PaymentForm = () => {
-  const { register, handleSubmit, watch, setFocus } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
-  const handleInputFocus = (evt) => {
-    setFocus(evt.target.name);
-  };
+export default function PaymentForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
 
   return (
-    <div>
-      <form className="flex flex-col gap-4 font-Gambetta text-black" onSubmit={handleSubmit(onSubmit)}>
-        <div className="m-auto w-[600px]">
-          <label className="block text-sm mb-1" htmlFor="mobileNumber">Card Number</label>
-          <input
-            type="number"
-            name="number"
-            placeholder="Card Number"
-            {...register("number")}
-            onFocus={handleInputFocus}
-            className="input input-bordered md:w-[600px] w-[485px] bg-[#D9D9D9] placeholder-black" 
-          />
-        </div>
-        <div className="m-auto w-[600px]">
-          <label className="block text-sm mb-1" htmlFor="mobileNumber">Name</label>
+    <form
+      className="flex flex-col gap-4 font-Gambetta text-black"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="m-auto w-[600px]">
+        <label className="block text-sm mb-1" htmlFor="name">
+          Name
+        </label>
+        <input
+          type="text"
+          placeholder="Name"
+          {...register("Name", { required: true, maxLength: 200 })}
+          className="input input-bordered md:w-[600px] w-[485px] bg-[#D9D9D9] placeholder-black"
+        />
+        {errors.Name && (
+          <span className="text-red-500">This field is required</span>
+        )}
+      </div>
+      <div className="m-auto w-[600px]">
+        <label className="block text-sm mb-1" htmlFor="cardNumber">
+          Card Number
+        </label>
+        <input
+          type="number"
+          placeholder="Card number"
+          {...register("Card number", {
+            required: true,
+            minLength: 16,
+            maxLength: 19,
+          })}
+          className="input input-bordered md:w-[600px] w-[485px] bg-[#D9D9D9] placeholder-black"
+        />
+        {errors["Card number"] && (
+          <span className="text-red-500">This field is required</span>
+        )}
+      </div>
+      <div className="flex space-x-[30px] m-auto">
+        <div>
+          <label className="block text-sm mb-1" htmlFor="expiryDate">
+            Expiry Date
+          </label>
           <input
             type="text"
-            name="name"
-            placeholder="Name"
-            {...register("name")}
-            onFocus={handleInputFocus}
-            className="input input-bordered md:w-[600px] w-[485px] m-auto bg-[#D9D9D9] placeholder-black"
+            placeholder="MM/YY"
+            {...register("Expiry date", {
+              required: true,
+              minLength: 3,
+              maxLength: 4,
+            })}
+            className="input input-bordered md:w-[285px] bg-[#D9D9D9] placeholder-black"
           />
+          {errors["Expiry date"] && (
+            <span className="text-red-500">This field is required</span>
+          )}
         </div>
-        <div className="flex space-x-[30px]  m-auto :">
-          <div>
-            <label className="block text-sm mb-1" htmlFor="mobileNumber">Expiry date</label>
-            <input
-              type="text"
-              name="expiry"
-              placeholder="MM/YY Expiry"
-              {...register("expiry")}
-              onFocus={handleInputFocus}
-              className="input input-bordered md:w-[285px] bg-[#D9D9D9] placeholder-black"
-            />
-          </div>
-          <div>
-            <label className="block text-sm mb-1" htmlFor="mobileNumber">CVC</label>
-            <input
-              type="number"
-              name="cvc"
-              placeholder="CVC"
-              {...register("cvc")}
-              onFocus={handleInputFocus}
-              className="input input-bordered md:w-[285px]  bg-[#D9D9D9] placeholder-black"
-            />
-          </div>
+        <div>
+          <label className="block text-sm mb-1" htmlFor="cvc">
+            CVC
+          </label>
+          <input
+            type="number"
+            placeholder="CVC"
+            {...register("CVC", { required: true, minLength: 3, maxLength: 3 })}
+            className="input input-bordered md:w-[285px] bg-[#D9D9D9] placeholder-black"
+          />
+          {errors.CVC && (
+            <span className="text-red-500">This field is required</span>
+          )}
         </div>
-      </form>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex justify-center mt-4">
-          <button type="submit" className="btn  rounded-full border-black text-black hover:text-white py-2 px-6 w-44 flex items-center gap-4 justify-center bg-gold mt-4">Submit</button>
-        </div>
-      </form>
-    </div>
+      </div>
+      <div className="flex justify-center mt-4">
+        <button
+          type="submit"
+          className="btn rounded-full border-black text-black hover:text-white py-2 px-6 w-44 flex items-center gap-4 justify-center bg-gold mt-4"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
   );
-};
-
-export default PaymentForm;
+}
